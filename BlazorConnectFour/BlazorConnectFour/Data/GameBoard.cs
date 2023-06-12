9ed3fdde1595ef2c725c2584ecd10b2fc16f78eb
Alex <blackwellag@appstate.edu>
2023-06-12 17:10:14 -0400

    work on connect4

---
+++ BlazorConnectFour/BlazorConnectFour/Data/GameBoard.cs
@@ -0,0 +1,38 @@
+﻿using BlazorConnectFour.Data;
+
+namespace BlazorConnectFour.Data
+{
+    public class GameBoard
+    {
+        public int Width { get; } = 7;
+        public int Height { get; } = 6;
+        public GamePiece[,] Board;
+
+        public GameBoard()
+        {
+            // set like this for memory speed
+            Board = new GamePiece[Width,Height];
+
+            // init all piecies to empty
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    Board[i, j] = new GamePiece(PieceColor.Blank);
+                }
+            }
+        }
+
+        public void Restart()
+        {
+            // reset all piecies to empty
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    Board[i, j].Color = PieceColor.Blank;
+                }
+            }
+        }
+    }
+}
